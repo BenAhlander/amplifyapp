bd19e1bed18e874eed4aa0822b2091f8e52f75c5
Ben Ahlander <ben.ahlander@gmail.com>
2021-05-07 16:45:38 -0600

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { API, Storage } from "aws-amplify";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import { listTodos } from "./graphql/queries";
 import {
   createTodo as createNoteMutation,
   deleteTodo as deleteNoteMutation,
 } from "./graphql/mutations";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Card from "./components/Card";
+import Nav from "./components/Nav";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import orange from "@material-ui/core/colors/red";
+import deepOrange from "@material-ui/core/colors/deepOrange";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepOrange[500],
+    },
+    secondary: {
+      main: orange[500],
+    },
+  },
+});
 
 const initialFormState = { name: "", description: "" };
 
@@ -65,34 +86,80 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={(e) =>
-          setFormData({ ...formData, description: e.target.value })
-        }
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <input type="file" onChange={onChange} />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{ marginBottom: 30 }}>
-        {notes.map((note) => (
-          <div key={note.id || note.name}>
-            <h2>{note.name}</h2>
-            <p>{note.description}</p>
-            <button onClick={() => deleteNote(note)}>Delete note</button>
-            {note.image && <img src={note.image} style={{ width: 400 }} />}
-          </div>
-        ))}
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Nav />
+        <h1>My Notes App</h1>
+        <Paper style={{ padding: "24px", maxWidth: "1000px", margin: "auto" }}>
+          <Grid container spacing={6}>
+            <Grid item xs={12} sm={6} md={4}>
+              <TextField
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                placeholder="Note name"
+                value={formData.name}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <TextField
+                onChange={(e) =>
+                  setFormData({ ...formData, description: e.target.value })
+                }
+                placeholder="Note description"
+                value={formData.description}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <input
+                accept="image/*"
+                id="contained-button-file"
+                multiple
+                type="file"
+                onChange={onChange}
+                style={{ display: "none" }}
+              />
+              <label htmlFor="contained-button-file">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  component="span"
+                  fullWidth
+                >
+                  Upload Image
+                </Button>
+              </label>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="secondary"
+                onClick={createNote}
+              >
+                Create Note
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+        <div style={{ padding: "24px" }}>
+          <Grid container spacing={6}>
+            {notes.map((note) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                <Card
+                  title={note.name}
+                  description={note.description}
+                  img={note.image}
+                  handleDelete={() => deleteNote(note)}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </div>
-      <AmplifySignOut />
-    </div>
+    </ThemeProvider>
   );
 }
 
